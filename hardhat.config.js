42a6351dea4fd97eca08eb762f74fddef44f6a3e
Vladimir Diamandi <vladdiamandi@gmail.com>
2022-05-31 11:00:59 +0300

    create NFT functionality

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+const { PRIVATE_KEY, MATIC_API_KEY } = process.env;
+module.exports = {
+  defaultNetwork: "PolygonMumbai",
+  networks: {
+    hardhat: {
+    },
+     PolygonMumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/v1/${MATIC_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
